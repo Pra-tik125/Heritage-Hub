f085e84c62d16ab8b17cacd7f181d0aa0290aa58
Pratik Ladva <144947009+Pra-tik125@users.noreply.github.com>
2024-10-18 00:46:41 +0530

    gallery added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,19 +2,19 @@
 function toggleMenu() {
     const nav = document.querySelector('.navdiv ul.nb1');
     nav.classList.toggle('active');
-  }
-  
-  // scroll event listener
-  window.addEventListener('scroll',()=>{
-        const nav = document.querySelector('.navdiv');
-        if (window.scrollY > 0) {
-            nav.classList.add('scrolled');
-          }
-        else {
-            nav.classList.remove('scrolled');
-        }
-  
-  })
+}
+
+// scroll event listener
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('.navdiv');
+    if (window.scrollY > 0) {
+        nav.classList.add('scrolled');
+    }
+    else {
+        nav.classList.remove('scrolled');
+    }
+
+})
 
 
 // current page navbar highlight
@@ -32,29 +32,48 @@ document.querySelectorAll('.navbar .nb1 li a').forEach(link => {
     }
 });
 
-  
-  // Form script
-  const form = document.getElementById('contactform');
-  const result = document.getElementById('result');
-  
-  form.addEventListener('submit', function (e) {
+// map popup
+function openMapModal() {
+    document.getElementById("mapModal").style.display = "block";
+}
+
+function closeMapModal() {
+    document.getElementById("mapModal").style.display = "none";
+}
+
+// Close the modal when clicking outside of it
+window.onclick = function(event) {
+    var modal = document.getElementById("mapModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+// Form script
+const form = document.getElementById('contactform');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function (e) {
     e.preventDefault();
-  
+
     // Captcha check
     const hCaptcha = form.querySelector('textarea[name=h-captcha-response]').value;
     if (!hCaptcha) {
         alert("Please fill out the captcha field");
         return;
     }
-  
+
     // Form data preparation
     const formData = new FormData(form);
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
-  
+
     result.innerHTML = "Please wait...";
     result.style.display = "block";  // Ensure the result message is visible
-  
+
     // Form submission
     fetch('https://api.web3forms.com/submit', {
         method: 'POST',
@@ -83,5 +102,4 @@ document.querySelectorAll('.navbar .nb1 li a').forEach(link => {
                 result.style.display = "none";
             }, 4000);
         });
-  });
-  
+});
